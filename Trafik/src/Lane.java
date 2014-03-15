a27f3a2469df13a9c6d38eeb6f49d19efc69dacd
Gabriel <gabriel.hajjar2@gmail.com>
2014-03-15 19:28:01 +0100

    Lane fixed!

---
+++ Trafik/src/Lane.java
@@ -0,0 +1,53 @@
+/* En fil */ 
+public class Lane {
+	Vehicle[] cars;
+	
+	public Lane(int length){
+		cars = new Vehicle[length];
+	}
+	
+	public Vehicle getFirst() {
+		if (cars[0] != null) {
+			return cars[0];
+		} else {
+			throw new RuntimeException("First spot is empty!");
+		}
+	}
+	
+	public boolean lastFree(){
+		if (cars[cars.length-1] == null){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void putLast(Vehicle v) {
+		if (lastFree()) {
+			cars[cars.length-1] = v;
+		} else {
+			throw new RuntimeException("Last spot is full!");
+		}
+	}
+	
+	public Vehicle removeFirst() {
+		if (getFirst() != null) {
+			Vehicle v = getFirst();
+			cars[0] = null;
+			return v;
+		}
+		return null;
+	}
+	
+	public void step() {
+		for (int a = 1; a < cars.length; a++) {
+			if (cars[a] != null && cars[a-1] == null) {
+				cars[a-1] = cars[a];
+				cars[a] = null;
+			}
+		}
+	}
+	
+	
+
+}
