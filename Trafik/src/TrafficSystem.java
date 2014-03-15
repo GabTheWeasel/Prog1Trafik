142986569b0721e5ab75cb8b174846c4e1545cb5
Gabriel <gabriel.hajjar2@gmail.com>
2014-03-15 21:39:29 +0100

    systemstuff

--- Trafik/src/TrafficSystem.java
+++ Trafik/src/TrafficSystem.java
@@ -6,10 +6,24 @@
 import java.util.ArrayList;
 
 public class TrafficSystem {
-
+	Lane r0;
+	Lane r1;
+	Lane r2;
+	Light s1;
+	Light s2;
+	VehicleGenerator vg;
+	ArrayList<Vehicle> queue = new ArrayList<Vehicle>();
  
   public TrafficSystem(VehicleGenerator vg) {
-
+	  this.vg = vg;
+	  Lane r0 = new Lane(9);
+	  Lane r1 = new Lane(7);
+	  Lane r2 = new Lane(7);
+	  
+	  Light s1 = new Light(5,2);
+	  Light s2 = new Light(5,2);
+	  
+	  
   }
   
   
@@ -18,7 +32,33 @@ public class TrafficSystem {
    * of components step methods
    */
   public void step() {
- 
+	  s1.step();
+	  s2.step();
+	  if (s1.isGreen()){
+		  r1.removeFirst();
+	  }
+	  if (s2.isGreen()) {
+		 r2.removeFirst(); 
+	  }
+	  r1.step();
+	  r2.step();
+	  
+	  if(r0.getFirst() != null) {
+		  if (r0.getFirst().getDestination() == 'S' && r2.lastFree()) {
+			  r2.putLast(r0.removeFirst());
+		  } else if (r0.getFirst().getDestination() == 'W' && r1.lastFree()) {
+			  r1.putLast(r0.removeFirst());
+		  }
+	  }
+	  r0.step();
+	  Vehicle temp = vg.step();
+	  if (temp != null) {
+		  queue.add(temp);
+	  }
+	  
+	  if(queue.size() > 0 && r0.lastFree()){
+		  r0.putLast(queue.remove(0));
+	  }
   }
   
   /**
